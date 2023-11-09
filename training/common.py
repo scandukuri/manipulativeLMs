f1c0d647ec148a1098e8345b73cf06b5fe2a12d0
Chinmaya Andukuri <andukuri@stanford.edu>
2023-11-09 01:25:56 -0800

    Revert "alpaca causal-lm support"
    This reverts commit df56ae530eac113eafd1bfec723b0fe871e9a0e0.

---
+++ training/common.py
@@ -0,0 +1,73 @@
+import pandas as pd
+from transformers import Trainer
+import argparse
+from datasets import load_dataset, load_metric
+import pandas as pd
+from collections import OrderedDict, Counter
+from typing import List, Tuple, Set, Dict, Any, Optional, NamedTuple, Iterator, Union
+from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM, AutoModelForCausalLM, DataCollatorForSeq2Seq, DataCollatorForLanguageModeling, Seq2SeqTrainingArguments, Seq2SeqTrainer, TrainingArguments, Trainer
+import nltk, os, csv, json, random
+import numpy as np
+import torch
+from tqdm import tqdm
+import json
+
+def set_seed(seed):
+    """ Set all seeds to make results reproducible """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+
+def tokenize(string, tokenizer, eos_id=None):
+    return tokenizer(string)
+
+def preprocess(examples, tokenizer, format_string):
+    source_target = [build(row, format_string)
+                  for _, row in pd.DataFrame(examples).iterrows()]
+    source = [tup[0] for tup in source_target]
+    target = [tup[1] if len(tup)>1 else "" for tup in source_target]
+    
+    model_inputs = tokenize(source, tokenizer) #tokenizer(source)
+
+    with tokenizer.as_target_tokenizer():
+        labels = tokenize(target, tokenizer) #tokenizer(target)
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+def decode(args, df, model, tokenizer, skip_special_tokens=True, remove_history=False):
+    model = model
+    is_greedy = (args.top_p == 0) and (args.top_k == 0) and (args.beams == 0)
+    
+    eos_token_id = tokenizer.encode("<eos>", add_special_tokens=False)[0]
+    generations = []
+    
+    for _, row in df.iterrows():
+        input_ids = torch.tensor([row['input_ids']], device='cuda')
+        
+        out = model.generate(
+                input_ids,
+                do_sample=args.beams == 0,
+                max_length=args.maxlen,
+                temperature=args.temperature,
+                top_p=args.top_p if args.top_p > 0 else None,
+                top_k=args.top_k if args.top_k > 0 else None,
+                num_beams=args.beams if args.beams > 0 else None,
+                early_stopping=True,
+                pad_token_id=50256,
+                no_repeat_ngram_size=3,
+                eos_token_id=eos_token_id
+            )
+        if remove_history:
+            generations.append(tokenizer.decode(out[:, input_ids.shape[-1]:][0], skip_special_tokens=skip_special_tokens))
+        
+        else:
+            generations.append(tokenizer.decode(out[0],
+                                            skip_special_tokens=skip_special_tokens
+                                           ))
+    return generations
---
+++ training/metrics.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+import bert_score
+import sacrebleu
+from rouge_score import rouge_scorer
+from glob import glob
+import argparse, re, nltk, os
+
+def person_split(txt):
+    return [x for x in re.split(r"(?=(PERSON|OTHER))",txt) if len(x) and x not in {'PERSON', 'OTHER'}]
+
+def person_subdivide(lst):
+    new_lst = []
+    for x in lst:
+        for y in person_split(x):
+            new_lst.append(y)
+    return new_lst
+
+def clean_constraints(txt):
+    
+    txt = re.sub("[\s]+’s", "'s", txt)
+    txt = re.sub("[\s]+'s", "'s", txt)
+    txt = re.sub("[\s]{2,}", " ", txt)
+    txt = re.sub("’", "'", txt)
+    txt = re.sub('<[\w//]+>', '', txt)
+    txt = re.sub(':', '', txt)
+    txt = re.sub('\[PERSON\]', 'PERSON', txt)
+    txt = re.sub('\[OTHER\]', 'OTHER', txt)
+    txt = re.sub('PERSON.s', "PERSON's", txt)
+    txt = re.sub("PERSON(' | ')(s|S)\b", "PERSON's", txt)
+    txt = re.sub(r"has not '", "is not '", txt)
+    
+    constraints = person_subdivide(txt.split('[AND]'))
+    
+    def clean_constraint(txt):
+        txt = re.sub('[\.//]', '', txt)
+        return txt.strip()
+    
+    cleaned = [clean_constraint(c) for c in constraints]
+    
+    out = [c for c in cleaned if len(c)]
+    if len(out):
+        return out
+    return ['']
+
+ROUGE_SCORER = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL', 'rougeLsum'], use_stemmer=True)
+def rouge_max(cands, refs):
+    scores = {}
+    
+    preprocess = lambda x : "\n".join(nltk.sent_tokenize(x.strip()))
+    for metric in ROUGE_SCORER.rouge_types:
+        max_scores = np.array([ max([ROUGE_SCORER.score(preprocess(ref), preprocess(cand))[metric].fmeasure 
+                                         for ref in refs
+                                    ])
+                                   for cand, refs in zip(cands, refs)
+                              ])
+        scores[metric] = np.mean(np.array(max_scores))
+    return scores
+
+def sacrebleu_max(cands, refs):
+    max_scores = np.array([ max([sacrebleu.corpus_bleu(cand, ref).score
+                                     for ref in refs
+                                ])
+                               for cand, refs in zip(cands, refs)
+                          ])
+    return np.mean(np.array(max_scores))
+
+
+# df --> cands, refs
+def get_cands_refs(df, prompt_col, cand_col, refs_col):
+    cand_list = []
+    refs_list = []
+    for prompt in set(df[prompt_col].values):
+        consider = df[df[prompt_col]==prompt].copy()
+        refs = list(consider[refs_col].values)
+        for cand in consider[cand_col].values:
+            cand_list.append(cand)
+            refs_list.append(refs)
+    return cand_list, refs_list
+
+def mean_length(sentences):
+    return np.mean(np.array([len(nltk.tokenize.word_tokenize(sent)) for sent in sentences]))
+
+def compute_metrics(df, prompt_col, cand_col, refs_col):
+    
+    cands, refs = get_cands_refs(df, prompt_col, cand_col, refs_col) 
+    scores = rouge_max(cands, refs)
+    bert_p, bert_r, bert_f1 = bert_score.score(cands, refs, lang='en')
+    scores['BERTScore_Precision'] = np.average(np.array(bert_p))
+    scores['BERTScore_Recall'] = np.average(np.array(bert_r))
+    scores['BERTScore'] = np.average(np.array(bert_f1))
+    scores['sacrebleu'] = sacrebleu_max(cands, refs)
+    scores['mean_length'] = mean_length(df[cand_col].values)
+    
+    return scores
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default='../results/*', help='glob regex for input models')
+    parser.add_argument('--output', type=str, help='path to directory for outputting results')
+    parser.add_argument('--target', default='constraints')
+    parser.add_argument('--prompt_col', type=str, default='setting-behavior')
+    args = parser.parse_args()
+
+    GEN_COL = 'constraints_generated'
+
+    data = {}
+    i=0
+    for dirname in list(glob(args.input)):
+        print(dirname)
+        for fn in list(glob(f"{dirname}/test_generations*.csv")):
+            print('\t', fn)
+
+            generations=pd.read_csv(fn)
+            generations[GEN_COL] = [' [AND] '.join(clean_constraints(c)) for c in generations[GEN_COL].values]
+            
+            print(generations[:10])
+
+            results = compute_metrics(generations, prompt_col=args.prompt_col, cand_col=GEN_COL, refs_col=args.target)
+
+            typ = "greedy"
+            if 'beams3' in fn:
+                typ='beam'
+            elif 'p0.9' in fn:
+                typ='p=0.9'
+
+            model = 'OTHER'
+            if 'bart' in fn:
+                model='bart'
+            elif 'gpt' in fn:
+                model='gpt'
+            elif 't5' in fn:
+                model='t5'
+
+            data[i] = results
+            data[i]['decoding'] = typ
+            data[i]['model'] = model
+            data[i]['fn'] = fn
+
+            print(data)
+
+            i+=1
+
+    out = pd.DataFrame().from_dict(data, orient='index').sort_values(['model', 'decoding'])
+
+    print(out)
+    out.to_csv(args.output)    
